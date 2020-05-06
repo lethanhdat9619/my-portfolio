ee32ae2feb276f23dc5a61f381beb04cf791234c
Le Thanh Dat (VTI.D2) <dat.lethanh@vti.vn>
2020-05-06 15:39:18 +0700

    doing UI personal information

--- src/Body.js
+++ src/Body.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import './Body.css';
-import { Nav, Image, Navbar, Row, Col } from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
 import projectImg from './images/project.png';
 import lineImg from './images/line.png';
 import faceImg from'./images/face.png';
+import lineImgCircle from './images/line2.png';
+import WOW from 'wow.js';
+
 class Body extends Component {
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         isShowContentBox: false
+    //     }
+    //     this.handleScroll = this.handleScroll.bind(this);
+    // }
+
+   componentWillMount() {
+    new WOW().init();
+   }
     render() {
         return (
             <div className="body-container">
                <nav>
                    <ul>
                        <li className="item item-info">
-                       <Image id="label-img" src={faceImg} roundedCircle width="16.5%"/>
+                       <Image id="label-img" src={faceImg} roundedCircle width="17%"/>
                        <span id="label-select">INFO</span>
                        </li>
                        <li className="item item-project">
@@ -29,13 +43,63 @@ class Body extends Component {
                    </ul>
                </nav>
 
-               <h1 class="animated bounceInDown delay-1s" id="title">INFOMATION</h1>
+               <h1 class="animated bounceInDown delay-0.5s" id="title">INFOMATION</h1>
+            
+               <Image src={lineImg} width="30%"/>
+                <p>Châm ngôn: Sau này, cần cù thì bù siêng năng, chỉ có làm thì mới có ăn, những cái loại không làm mà muốn có ăn thì ...</p>
+                <p>Triết lý: Liều nhiều thì ăn nhiều, liều ít thì ăn ít, không liều thì không có mà ăn</p>
+               <Image src={lineImgCircle} width="30%"/>
+
+                <h1 id="title">TIMELINE</h1>
+                <div className="timeline-container">
+                    <div class="wow bounceInRight" id="timeline-item">
+                        <div className="timeline-sequence"></div>
+                        <div className="timeline-box boxInRightSide">
+                            <h2>2015</h2>
+                            <p id="timeline-content"> Vào học tại Học viện Công nghệ Bưu chính Viễn thông</p>
+                        </div>
+                    </div>
+                    <div class="wow bounceInLeft" id="timeline-item">
+                        <div className="timeline-sequence"></div>
+                        <div className="timeline-box boxInLeftSide ">
+                            <h2>2016</h2>
+                            <p id="timeline-content">Thi lại môn C++</p>
+                        </div>
+                    </div>
+                    <div class="wow bounceInRight" id="timeline-item">
+                        <div className="timeline-sequence"></div>
+                        <div className="timeline-box boxInRightSide ">
+                            <h2>2017</h2>
+                            <p id="timeline-content">Tiếp tục thi lại môn C++</p>
+                        </div>
+                    </div>
+                    <div class="wow bounceInLeft" id="timeline-item">
+                        <div className="timeline-sequence"></div>
+                        <div className="timeline-box boxInLeftSide ">
+                            <h2>2017</h2>
+                            <p id="timeline-content">Thi qua C++</p>
+                        </div>
+                    </div>
+                    <div class="wow bounceInRight" id="timeline-item">
+                        <div className="timeline-sequence"></div>
+                        <div className="timeline-box boxInRightSide ">
+                            <h2>2019</h2>
+                            <p id="timeline-content">Tốt nghiệp</p>
+                        </div>
+                    </div>
+                </div>
+
+                <Image src={lineImgCircle} width="30%"/>
+
+                <h1 id="title">HOBBIES</h1>
+
+            
                
-               <Image src={lineImg} width="20%"/>
             </div>
            
         );
     }
+
 }
 
 export default Body;
